5ba03c9ae9bf76dc714439e911b7623028cdd5d1
Milia-K <milyamilya20@gmail.com>
2023-07-17 23:50:13 +0300

    first commit

--- script.js
+++ script.js
@@ -1,16 +1,63 @@
-let weight=document.getElementById('weight');
-let height=document.getElementById('height');
-let btn=document.getElementById('button');
-let result=document.querySelector('#result');
+//
+let grade=document.getElementById('grade');
+let average=document.getElementById('average');
+let max=document.getElementById('max');
+let min=document.getElementById('min');
+let negative =document.getElementById('negative'); 
+let letter=document.getElementById('letter');
 
-function calculateBMI(){
-    let w=weight.value;
-    let h= height.value/100;
-    let x=Math.pow(h,2);
-    let y=w/x;
-    let z=y.toFixed(2);
-    console.log(z);
-    result.textContent = z;  
+
+function calculate(){
+
+let grades = [];
+console.log(grades);
+
+//random grades
+for (let i = 0; i < 12; i++) {
+  grades.push(Math.floor(Math.random() * 100) + 1);
 }
 
-btn.addEventListener('click', calculateBMI);
+//average grade
+let sum = grades.reduce((item, current) => item + current);
+let averageGrade = Math.floor(sum / grades.length);
+
+//max grade
+let maxGrade = Math.max(...grades);
+
+//min grade
+let minGrade = Math.min(...grades);
+
+//positive grade
+let positiveGrade = grades.filter(grade => grade >= 60);
+let positiveCount = positiveGrade.length;
+
+//negative grade
+let negativeGrade = grades.filter(grade => grade < 60);
+let negativeCount = negativeGrade.length;
+
+//to letter grades
+let letterGrades = grades.map(grade => {
+if (grade >= 80) {
+        return "A";
+    } else if (grade >= 60) {
+        return "B";
+    } else if (grade >= 40) {
+        return "C";
+    } else if (grade >= 20) {
+        return "D";
+    } else {
+        return "E";
+    }
+});
+
+//results
+grade.textContent = grades;  
+average.textContent = averageGrade;  
+max.textContent = maxGrade;  
+min.textContent = minGrade;  
+positive.textContent = positiveCount;  
+negative.textContent = negativeCount;  
+letter.textContent = letterGrades;  
+}
+
+btn.addEventListener('click', calculate);
